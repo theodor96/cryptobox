11d2eb62a3e040b9e6ec392dd805354f2e17bbec
Theodor Serbana <theodor.serbana96@gmail.com>
2020-03-31 03:24:10 +0300

    Introduce new headers

---
+++ src/All.h
@@ -0,0 +1,13 @@
+#ifndef CRYPTOBOX_SRC_ALL_H
+#define CRYPTOBOX_SRC_ALL_H
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Buffer.h"
+#include "KeyHandle.h"
+#include "Message.h"
+#include "Operations.h"
+#include "Signature.h"
+#include "Types.h"
+
+#endif
