7511952f91ee7b1e5973328c31192b3bc03bd16d
Theodor Serbana <theodor.serbana96@gmail.com>
2020-03-31 09:54:18 +0300

    Add new public key / signature specific test data

--- src/Client.cpp
+++ src/Client.cpp
@@ -13,16 +13,16 @@ namespace
     constexpr const char* KEY_PASSPHRASE = "my very secret passphrase";
     constexpr const char* MESSAGE_TO_SIGN = "some message to be signed with ECDSA via brainpoolp256r1";
 
-    constexpr const char* SPECIFIC_PUBLIC_KEY_HEX = "5eee58f650e1c834be271c7311ff36200ce4ec990fc5158089b93e994861c1c29d"
-                                                    "92a0ee458d85cef0f48f77cb669dbe2bc6cf4aa358bcdcadeed7135adc3a19";
+    constexpr const char* SPECIFIC_PUBLIC_KEY_HEX = "76af84267752a731424b798faba4c5778ec40d1fccc6835a3e322db42db525505d"
+                                                    "c93d93468d0017a418c19446410fce766c7a403d7ad1e52a1bc1bf8ecc6081";
 
-    constexpr const char* SPECIFIC_VALID_SIGNATURE_HEX = "304402202d8cf06b2d06ad6db495e92def8af3311a58d94d3ab24ead49be8"
-                                                         "428385cb8d20220173ba3b9832796ff40914b538803f719d67812705ad847"
-                                                         "e379321ff5194b035f";
+    constexpr const char* SPECIFIC_VALID_SIGNATURE_HEX = "30440220102443fd266c93a6d2731e71ddc1178d18b55b540e4143df97f"
+                                                         "f9123d401510302201ca0e031e42fa9a968a5a52b55bdda14303d480555"
+                                                         "b07b27cb4120dc6851f4c7";
 
-    constexpr const char* SPECIFIC_INVALID_SIGNATURE_HEX = "304402202d8cf06b2d06ad6db495e92def8af3311a58d94d3ab24ead49b"
-                                                           "e8428385cb8d20220173ba3b9832796ff40914b538803f719d67812705a"
-                                                           "d847e379321ff5194b035e"; // last digit changed
+    constexpr const char* SPECIFIC_INVALID_SIGNATURE_HEX = "30440220102443fd266c93a6d2731e71ddc1178d18b55b540e4143df97f"
+                                                           "f9123d401510302201ca0e031e42fa9a968a5a52b55bdda14303d480555"
+                                                           "b07b27cb4120dc6851f4c6"; // last digit changed
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
